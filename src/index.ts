900ba7f8968b728085dbed15a750fc1462d623c2
zohaib ali <zohaib24a@gmail.com>
2026-04-03 21:43:48 +0500

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpAgent } from "agents/mcp";
+import { z } from "zod";
+
+// Define our MCP agent with tools
+export class MyMCP extends McpAgent {
+	server = new McpServer({
+		name: "Authless Calculator",
+		version: "1.0.0",
+	});
+
+	
+}
+
+export default {
+	fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const url = new URL(request.url);
+
+		if (url.pathname === "/mcp") {
+			return MyMCP.serve("/mcp").fetch(request, env, ctx);
+		}
+
+		return new Response("Not found", { status: 404 });
+	},
+};
